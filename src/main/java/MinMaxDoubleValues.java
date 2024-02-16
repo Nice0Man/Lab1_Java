b35b2f4bbb146d0c20bdbd6877ade92a0f768353
Eugene <e.krotov03@gmail.com>
2024-02-16 20:17:49 +0300

    fourth task

---
+++ src/main/java/MinMaxDoubleValues.java
@@ -0,0 +1,9 @@
+public class MinMaxDoubleValues {
+    public static void main(String[] args) {
+        double smallestDouble = Math.nextUp(0.0); // Наименьшее положительное значение
+        double largestDouble = Double.MAX_VALUE; // Наибольшее положительное значение
+
+        System.out.println("Наименьшее положительное значение double: " + smallestDouble);
+        System.out.println("Наибольшее положительное значение double: " + largestDouble);
+    }
+}
