4eafc7399ee8cd5cf5445eff932d210d8bfda713
Eugene <e.krotov03@gmail.com>
2024-02-16 20:28:46 +0300

    fifth task

---
+++ src/main/java/DoubleToIntConversion.java
@@ -0,0 +1,11 @@
+public class DoubleToIntConversion {
+    public static void main(String[] args) {
+        double bigDoubleValue = Integer.MAX_VALUE * 2.0;
+
+        // Попытка приведения к int
+        int intValue = (int) bigDoubleValue;
+
+        System.out.println("Double value: " + bigDoubleValue);
+        System.out.println("Int value after conversion: " + intValue);
+    }
+}
