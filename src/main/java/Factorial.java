9397907a1371e99bb78b3603c2f54405bf87dcf3
Eugene <e.krotov03@gmail.com>
2024-02-16 20:34:26 +0300

    sixth task

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+public class Factorial {
+    private static BigInteger calculate(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int n = 1000;
+        // Вычисление факториала с использованием класса BigInteger
+        BigInteger factorial = calculate(n);
+
+        System.out.printf("Факториал %d: ",n);
+        System.out.println(factorial);
+    }
+}
