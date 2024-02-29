38139b51e47d2d7fbff52f459bb7e741da49ca20
Eugene <e.krotov03@gmail.com>
2024-02-29 19:17:18 +0300

    12th task

---
+++ src/main/java/lab_1/tasks/Label.java
@@ -0,0 +1,37 @@
+package lab_1.tasks;
+
+public class Label {
+    public static void labelMethod(){
+        outer:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i * j > 6) {
+                    System.out.println("Breaking outer loop");
+                    break outer; // Прерывание внешнего цикла с меткой outer если значение больше 6
+                }
+                System.out.println(i + " * " + j + " = " + (i * j));
+            }
+        }
+    }
+    public static void noLabelMethod(){
+        boolean flag = false;
+        for (int i = 0; i < 5; i++) {
+            if (flag){
+                break;
+            }
+            for (int j = 0; j < 5; j++) {
+                if (i * j > 6) {
+                    System.out.println("Breaking outer loop");
+                    flag = true;
+                    break; // Прерывание внешнего цикла с меткой если значение больше 6
+                }
+                System.out.println(i + " * " + j + " = " + (i * j));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        labelMethod();
+        System.out.println("----------------------------------------------------");
+        noLabelMethod();
+    }
+}
