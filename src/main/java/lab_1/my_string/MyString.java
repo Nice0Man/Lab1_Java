a48cf9832dbe2eef3918b5de03c0bfc52d40ea0e
Eugene <e.krotov03@gmail.com>
2024-02-29 16:56:43 +0300

    join works

--- src/main/java/lab_1/my_string/MyString.java
+++ src/main/java/lab_1/my_string/MyString.java
@@ -1,9 +1,8 @@
 package lab_1.my_string;
 
-import java.io.OutputStream;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Optional;
 
 public final class MyString{
     private byte[] stringByteArray;
@@ -12,16 +11,15 @@ public final class MyString{
         return stringByteArray;
     }
     public void setStringByteArray(byte[] ba) {
-        this.stringByteArray = Arrays.copyOf(ba,ba.length);
+        this.stringByteArray = new byte[ba.length];
+        System.arraycopy(ba,0,this.stringByteArray,0 ,ba.length);
     }
-
-    public void setStringByteArray(ArrayList<byte[]> stringArrayList){
-        for (int i = 0; i <stringArrayList.toArray().length ; i++) {
-
-        }
+    public void setStringByteArray(MyString s) {
+        this.stringByteArray = new byte[s.length()];
+        System.arraycopy(s.getStringByteArray(),0,this.stringByteArray,0 ,s.length());
     }
     public MyString() {
-        this.stringByteArray = null;
+        this.stringByteArray = new byte[0];
     }
     public MyString(String s) {
         if (s == null){
@@ -29,42 +27,62 @@ public final class MyString{
         }
         this.setStringByteArray(s.getBytes());
     }
-
     public int length(){
+        if (this.stringByteArray == null){
+            throw new NullPointerException("Byte array is null!");
+        }
         return this.toByteArray().length;
     }
-
     public byte[] toByteArray() {
         return this.stringByteArray;
     }
+    public boolean isEmpty(){
+        return this.stringByteArray.length == 0;
+    }
+    public MyString concat(MyString s){
+        if (s == null || s.isEmpty()) {
+            throw new NullPointerException("Cannot concatenate with a null string.");
+        }
+        byte[] thisBytes = this.toByteArray();
+        byte[] otherBytes = s.toByteArray();
 
-    public MyString join(MyString delimiter, MyString... strings) {
-        for (MyString str : strings) {
-            this.stringArrayList.addLast(str.toByteArray());
-            this.stringArrayList.addLast(delimiter.toByteArray());
+        byte[] resultBytes = new byte[thisBytes.length + otherBytes.length];
+        System.arraycopy(thisBytes, 0, resultBytes, 0, thisBytes.length);
+        System.arraycopy(otherBytes, 0, resultBytes, thisBytes.length, otherBytes.length);
+
+        this.setStringByteArray(resultBytes);
+        return this;
+    }
+    public MyString concat(String s){
+        if (s == null || s.isEmpty()) {
+            throw new NullPointerException("Cannot concatenate with a null string.");
         }
-        // Удаляем последний добавленный разделитель
-        if (strings.length > 0) {
-            this.stringArrayList.removeLast();
-        }
+        byte[] thisBytes = this.toByteArray();
+        byte[] otherBytes = s.getBytes();
+
+        byte[] resultBytes = new byte[thisBytes.length + otherBytes.length];
+        System.arraycopy(thisBytes, 0, resultBytes, 0, thisBytes.length);
+        System.arraycopy(otherBytes, 0, resultBytes, thisBytes.length, otherBytes.length);
+
+        this.setStringByteArray(resultBytes);
         return this;
     }
     public MyString join(String delimiter, String... strings) {
-        this.stringArrayList = new ArrayList<byte[]>();
-        for (String str : strings) {
-            if (str != null){
-                this.stringArrayList.addLast(str.getBytes());
-                this.stringArrayList.addLast(delimiter.getBytes());
+        MyString result = new MyString();
+
+        for (int i = 0; i < strings.length; i++) {
+            MyString tmpStr = new MyString(strings[i]);
+            if (!tmpStr.isEmpty()){
+                result.concat(tmpStr);
+
+                if (i < strings.length - 1 && strings[i + 1] != null && !strings[i + 1].isEmpty()) {
+                    result.concat(delimiter);
+                }
             }
         }
-        // Удаляем последний добавленный разделитель
-        if (strings.length > 0) {
-            this.stringArrayList.removeLast();
-        }
-        this.setStringByteArray(stringArrayList);
+        this.setStringByteArray(result);
         return this;
     }
-
     public MyString trim(){
         int startIndex = 0;
         int endIndex = this.length();
@@ -74,8 +92,10 @@ public final class MyString{
         while (endIndex > startIndex && this.stringByteArray[endIndex - 1] == ' ') {
             endIndex--;
         }
-        byte[] tmp = Arrays.copyOfRange(this.stringByteArray, startIndex, endIndex);
-        this.setStringByteArray(tmp);
+        int length = endIndex - startIndex;
+        byte[] tmpArray = new byte[length];
+        System.arraycopy(this.stringByteArray, startIndex, tmpArray, 0, length);
+        this.setStringByteArray(tmpArray);
         return this;
     }
 
@@ -103,7 +123,7 @@ public final class MyString{
 //    }
     @Override
     public String toString() {
-        return new String(stringByteArray);
+        return new String(this.stringByteArray);
     }
     public boolean includeSymbol(char symbol) {
         for (int i = 0; i < this.length(); i++){
@@ -116,15 +136,16 @@ public final class MyString{
 
     public static void main(String[] args) {
         MyString customString = new MyString("   Hello, World!   ");
-        System.out.println("New String: " + customString);
-        System.out.println("Trimmed String: " + customString.trim());
+        System.out.println("New String: " + customString );
+        System.out.println("Trimmed String: |" + customString.trim() + "|");
 
-        customString.join("-", "Java", "is", "awesome");
+        customString.concat(" Java is awesome!");
+        System.out.println("Concatenated String: " + customString);
+
+        customString.join("-","JavaScript", "for", "kids!");
         System.out.println("Joined String: " + customString);
-
 //        String[] splitArray = customString.split("-");
 //        System.out.println("Split String: " + Arrays.toString(splitArray));
-        System.out.println("Joined String: " + customString.indexOf("-".getBytes()));
 
         byte[] byteArray = customString.toByteArray();
         System.out.println("String to ByteArray: " + Arrays.toString(byteArray));
