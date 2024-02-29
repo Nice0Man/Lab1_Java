542c5677c1e05e32a71cf40311852caf9537f78f
Eugene <e.krotov03@gmail.com>
2024-02-29 17:50:56 +0300

    create empty tests

---
+++ src/test/java/lab_1/my_string/MyStringTest.java
@@ -0,0 +1,74 @@
+package lab_1.my_string;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStringTest {
+
+    @org.junit.jupiter.api.Test
+    void getStringByteArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setStringByteArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetStringByteArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void length() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toByteArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void concat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testConcat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void join() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void trim() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void indexOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testIndexOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void substring() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void split() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSplit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void includeSymbol() {
+    }
+}
